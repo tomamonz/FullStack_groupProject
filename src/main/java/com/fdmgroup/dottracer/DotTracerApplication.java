0af14cfd96503d57b699ef7ee154e7bad79ce95a
Mateusz Mikrut <mateusz.mikrut@fdmgroup.com>
2023-06-05 16:33:22 +0200

    initial commit

---
+++ src/main/java/com/fdmgroup/dottracer/DotTracerApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.dottracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotTracerApplication.class, args);
+	}
+
+}
