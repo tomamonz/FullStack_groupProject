584c526e076a42f0e4836b2ee0a4764867bf5901
Mateusz Mikrut <mateusz.mikrut@fdmgroup.com>
2023-06-05 18:50:26 +0200

    added ParcelService

--- DotTracerClient/src/main/java/com/fdmgroup/dottracer/repository/ParcelRepository.java
+++ DotTracerClient/src/main/java/com/fdmgroup/dottracer/repository/ParcelRepository.java
@@ -1,10 +1,12 @@
 package com.fdmgroup.dottracer.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.fdmgroup.dottracer.model.Parcel;
 
 public interface ParcelRepository extends CrudRepository<Parcel, Long> {
 
-	Optional<Parcel> findByParcelNumber(Long parcelNumber);
+	public Optional<Parcel> findByParcelNumber(Long parcelNumber);
 }
---
+++ DotTracerClient/src/main/java/com/fdmgroup/dottracer/service/ParcelService.java
@@ -0,0 +1,19 @@
+package com.fdmgroup.dottracer.service;
+
+import java.util.Optional;
+
+import com.fdmgroup.dottracer.model.Parcel;
+
+public interface ParcelService {
+	public Optional<Parcel> findByParcelNumber(Long parcelNumber);
+
+	public Parcel addParcel(Parcel parcel);
+
+	public Iterable<Parcel> findAll();
+
+	public Optional<Parcel> findById(Long id);
+
+	public Optional<Parcel> updateParcel(Parcel parcel);
+
+	public void removeParcelById(Long id);
+}
