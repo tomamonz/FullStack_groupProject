2b39f63c9bef6d44dc19c58de50eb19d77908b55
Tom Zal <Tomasz.Zaleski@fdmgroup.com>
2023-06-05 21:14:48 +0200

    added based of gateway and done eureka server

---
+++ DotTrackerGateway/gatewaydottracker/src/test/java/com/fdmgroup/gatewaydottracker/GatewaydottrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.gatewaydottracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaydottrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
